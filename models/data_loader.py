88278505a77d3f4ab706524ec5e6ae3620d15864
adwait <adwait.d10@gmail.com>
2019-06-01 19:22:21 +0530

    Working on a small subset of CIFAR10 data for testing

--- models/data_loader.py
+++ models/data_loader.py
@@ -1,12 +1,11 @@
-import os
-import torch
 import torchvision
-
-from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from torch.utils.data.sampler import SubsetRandomSampler
+
 
 class CIFARLoader():
-    def __init__(self, dataset_type='CIFAR10', root='./data/CIFAR10', transform_list = [transforms.ToTensor()]):
+    def __init__(self, dataset_type='CIFAR10', root='./data/CIFAR10', transform_list=[transforms.ToTensor()]):
         self.transform_list = transform_list
         self.dataloader = {}
         self.classlist = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
@@ -16,13 +15,13 @@ class CIFARLoader():
                                 root=root,
                                 train=True,
                                 download=True,
-                                transform = transforms.Compose(self.transform_list)
+                                transform=transforms.Compose(self.transform_list)
                                 )
             self.test_set = torchvision.datasets.CIFAR10(
                                 root=root,
                                 train=False,
                                 download=True,
-                                transform = transforms.Compose(self.transform_list)
+                                transform=transforms.Compose(self.transform_list)
                                 )
 
     def get_dataloader(self, batch_size):
@@ -32,11 +31,13 @@ class CIFARLoader():
         """
         for split in ['train', 'test']:
             if split == 'train':
-                self.dataloader[split] = DataLoader(self.train_set, batch_size=batch_size, shuffle=True)
+                self.dataloader[split] = DataLoader(self.train_set, batch_size=batch_size,
+                                                    sampler=SubsetRandomSampler(list(range(10000))))
             if split == 'test':
-                self.dataloader[split] = DataLoader(self.test_set, batch_size=batch_size, shuffle=True)
+                self.dataloader[split] = DataLoader(self.test_set, batch_size=batch_size,
+                                                    sampler=SubsetRandomSampler(list(range(10000))))
 
         return self.dataloader
 
-    def get_class_list():
+    def get_class_list(self):
         return self.classlist
--- train.py
+++ train.py
@@ -1,27 +1,26 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torchvision
-from torch.autograd.variable import Variable
 import torchvision.transforms as transforms
+from torch.autograd.variable import Variable
 
-import matplotlib.pyplot as plt
-
-from models.model import Net
 from models.data_loader import CIFARLoader
+from models.model import Net
+
 
 def train(model, datasetloader, loss_fn, optimizer, batch_size=4, num_epochs=3):
     """ Train on the dataset for num_epochs printing the loss for every epoch"""
 
-    dataloader = datasetloader.get_dataloader(batch_size)['train'] # load train_set
+    dataloader = datasetloader.get_dataloader(batch_size)['train']  # load train_set
 
     # set model in training mode
     model.train()
     running_loss = 0.0
+    loss = 0
     for epoch in range(num_epochs):
         for batch_idx, (images, labels) in enumerate(iter(dataloader)):
-            optimizer.zero_grad() # Reset gradients in every iteration
-            train_batch, train_labels = Variable(images), Variable(labels) 
+            optimizer.zero_grad()  # Reset gradients in every iteration
+            train_batch, train_labels = Variable(images), Variable(labels)
 
             output_batch = model(train_batch)
             loss = loss_fn(output_batch, train_labels)
@@ -29,54 +28,51 @@ def train(model, datasetloader, loss_fn, optimizer, batch_size=4, num_epochs=3):
             optimizer.step()
 
             running_loss += loss.item()
-            if(batch_idx % 1000 == 999):
+            if batch_idx % 1000 == 999:
                 print("Epoch:{} Batch:{} loss: {}".format(epoch + 1, batch_idx + 1, running_loss / 2000))
                 running_loss = 0.0
 
-        print("Epoch {} done with loss {}".format(epoch, loss))
+        print("Epoch {} done with loss {}".format(epoch + 1, loss))
 
 
 def evaluate(model, datasetloader, loss_fn, batch_size=4):
-    
     dataloader = datasetloader.get_dataloader(batch_size)['test']
     model.eval()
 
     running_loss = 0.0
     with torch.no_grad():
         for data_batch, labels_batch in dataloader:
-
             # fetch the next evaluation batch
             data_batch, labels_batch = Variable(data_batch), Variable(labels_batch)
-            
+
             # compute model output
             output_batch = model(data_batch)
             loss = loss_fn(output_batch, labels_batch)
             running_loss += loss.item()
-            print("Accuracy: {}".format(accuracy(output_batch, labels_batch)))
+            print("Accuracy: ", accuracy(output_batch, labels_batch))
+
 
 def accuracy(outputs, labels):
     _, pred = torch.max(outputs.data, dim=1)
-    return torch.sum(output == labels) / labels.size
+    acc_sum = torch.sum(pred == labels)
+    labels_len = float(len(labels))
+    return acc_sum.item() / labels_len
 
-if __name__ == '__main__':
-    params = {}
-    params["shape"] = (32, 32, 3) 
-    params["out_channels"] = 10
-    params["pool_size"] = 2
-    params["class_num"] = 10
-    params["kern_size"] = 5
-    params["batch_norm"] = True
+
+if __name__ == "__main__":
+    params = {"shape": (32, 32, 3), "out_channels": 10, "pool_size": 2, "class_num": 10, "kern_size": 5,
+              "batch_norm": True}
 
     net = Net(params)
 
     # Specify transformations by the image
-    transform_list = [transforms.ToTensor(), 
-            transforms.Normalize((0.5, 0.5, 0.5) , (0.5, 0.5, 0.5))
-            ]
+    transform_list = [transforms.ToTensor(),
+                      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                      ]
 
     dataloader = CIFARLoader(transform_list=transform_list)
     loss_fn = nn.CrossEntropyLoss()
     optimizer = optim.SGD(net.parameters(), lr=1e-3, momentum=0.9)
 
-    train(net, dataloader, loss_fn, optimizer, 16, 5)
-    evaluate(net, dataloader, loss_fn, 16)
+    train(net, dataloader, loss_fn, optimizer, 4, 3)
+    evaluate(net, dataloader, loss_fn, 4)
